082fbcaee49e382dcc13c598bc754a200a53fec1
James Edwards <jamese@syapse.com>
2016-08-11 12:56:26 -0700

    more error checking on addTable method of db.js

--- src/database/db.js
+++ src/database/db.js
@@ -7,6 +7,9 @@ class Opal {
     this.listeners = {}
   }
   addTable (name) {
+    if (!name || typeof name !== 'string') {
+      throw new Error(`The addTable method requires a table name that is a string.`)
+    }
     if (this.tables[name] !== undefined) {
       throw new Error(`A table with the name ${name} already exists`)
     }
